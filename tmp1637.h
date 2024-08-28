2c22ff49c667361881b6f3a9b670275e4c967c39
Sergey Matveychuk <sem@semmy.ru>
2024-08-28 17:46:18 +0300

    Add codes for segments for TM1637 displays

---
+++ tmp1637.h
@@ -0,0 +1,18 @@
+//
+//      A
+//     ---
+//  F |   | B
+//     -G-
+//  E |   | C
+//     ---
+//      D
+
+#define SEG_A		0b00000001
+#define SEG_B		0b00000010
+#define SEG_C		0b00000100
+#define SEG_D		0b00001000
+#define SEG_E		0b00010000
+#define SEG_F		0b00100000
+#define SEG_G		0b01000000
+#define SEG_DP		0b10000000    // : set on digit 1
+#define SEG_COLON	0b10000000    // : set on digits 0, 2, 3
